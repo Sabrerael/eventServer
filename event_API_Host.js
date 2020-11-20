f334bef10156cfce38cf11141f72e084409b552a
Andrew Bentley <sabrerael@hotmail.com>
2020-11-19 20:01:40 -0700

    Added basic responses for additional API calls

--- event_API_Host.js
+++ event_API_Host.js
@@ -3,10 +3,28 @@ var events = require('./events');
 var app = express();
 var fs = require("fs");
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res, next) {
     console.log( events.toString() );
     res.end( events.toString() );
-})
+});
+
+app.get('/search/:input', function (req, res, next) {
+    var input = req.params.input;
+    console.log( input );
+    res.end( input );
+});
+
+app.get('/location/:location', function (req, res, next) {
+    var location = req.params.location;
+    console.log( location );
+    res.end( location );
+});
+
+app.get('/date/:date', function (req, res, next) {
+    var date = req.params.date;
+    console.log( date );
+    res.end( date );
+});
 
 var server = app.listen(8081, function () {
     var host = server.address().address
